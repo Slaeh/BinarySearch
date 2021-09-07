a5a3542bdac580e6dae66fdfb3eeec17cf5f3951
Harjit <harjitliyal@gmail.com>
2021-09-07 17:03:28 -0400

    new problem

---
+++ Medium/NLexicographicIntegers.js
@@ -0,0 +1,10 @@
+class Solution {
+  solve(n) {
+    let arr = [];
+    for (let i = 1; i <= n; i++) {
+      arr.push(i);
+    }
+    return arr.sort();
+  }
+}
+//https://binarysearch.com/problems/N-Lexicographic-Integers
